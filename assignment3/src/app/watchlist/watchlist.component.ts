7aa37d30c55e335424b68569dc6fb71f9ed9856f
sawant-nidhish <57409535+sawant-nidhish@users.noreply.github.com>
2024-03-16 17:57:18 -0700

    Workig app

--- assignment3/src/app/watchlist/watchlist.component.ts
+++ assignment3/src/app/watchlist/watchlist.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
-
+import { Router, ActivatedRoute} from '@angular/router';
+import {CompanyDescriptionService} from '../services/company-description.service'
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
@@ -9,35 +10,74 @@ import { catchError } from 'rxjs/operators';
 })
 export class WatchlistComponent {
 watchlist:any
-constructor(private http: HttpClient){
+watchListData:any=[]
+isEmpty:boolean=false
+isLoading:boolean=true
+constructor(private http: HttpClient,private route: ActivatedRoute,
+  private router: Router, private companyDataApi: CompanyDescriptionService){
   
 }
 
-ngOnInit(){
-  this.checkInWatchList()  
+async ngOnInit() {
+  this.isLoading=true
+  try {
+    this.watchlist = await new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.checkInWatchList());
+      }, 1000);
+    });
+    // setTimeout(() => {
+      
+    // }, 10000);
+    // console.log(this.watchlist);
+    if (this.watchlist) {
+      for (let item in this.watchlist) {
+        this.companyDataApi.getCompanyPrice(this.watchlist[item].name).subscribe(data=>{
+          this.watchListData.push({
+            name:this.watchlist[item].name,
+            companyName:this.watchlist[item].companyName,
+            c:data.c,
+            d:data.d,
+            dp:data.dp
+          })
+          // console.log(data)
+        })
+      }
+      console.log(this.watchListData);
+      this.isEmpty=false
+      this.isLoading=false
+    } else {
+      console.log("Watchlist is empty");
+      this.isEmpty=true
+      this.isLoading=false
+    }
+  } catch (error) {
+    console.error("Error fetching watchlist:", error);
+  }
 }
 
-checkInWatchList(){
-  let response=this.http.get(`http://localhost:3000/api/watchlist`).pipe(
-  catchError(error => {
-    console.error('Error fetching company data:', error);
-    throw error;
-  })
-);
-response.subscribe(data=>{
-  if(Object.keys(data).length>0){
-    this.watchlist=data
-  }
-  else{
-    
-  }
-    console.log("This is the data from the wathclist endpoint",Object.keys(data).length)
-  
-})
+async checkInWatchList() {
+  return new Promise<any>((resolve, reject) => {
+    this.http.get('http://localhost:3000/api/watchlist').subscribe(
+      (data) => {
+        if (Object.keys(data).length > 0) {
+          console.log("Watchlist data received:", data);
+          resolve(data);
+        } else {
+          console.log("Watchlist is empty");
+          resolve(null);
+        }
+      },
+      (error) => {
+        console.error('Error fetching watchlist:', error);
+        reject(error);
+      }
+    );
+  });
 }
 
-deleteFromWatchList(){
-  const response=this.http.delete(`http://localhost:3000/api/watchlist?symbol="AAPL"`).pipe(
+async deleteFromWatchList(symbol:string){
+  const response=this.http.delete(`http://localhost:3000/api/watchlist?symbol=${symbol}`).pipe(
     catchError(error => {
       console.error('Error fetching company data:', error);
       throw error;
@@ -46,6 +86,20 @@ deleteFromWatchList(){
   response.subscribe(data=>{
     console.log("This is the data from the wathclist endpoint",data)
   });
+}
+
+removeItem(removedItem:any){
+  console.log(removedItem.name)
+  this.watchListData = this.watchListData.filter((item:any) => item!=removedItem);
+  this.deleteFromWatchList(removedItem.name)
+  if(this.watchListData.length==0){
+    this.isEmpty=true
+  }
+  // console.log(this.watchListData)
+}
+
+redirect(item:any){
+  this.router.navigate(['/search', item.name]);
 }
 }
 
